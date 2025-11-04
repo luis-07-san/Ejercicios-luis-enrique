c7f5e23932e9b170365e5008fc8b56ac7a38dfe9
luis enrique <luisenriquesanchezjimenez689@gmail.com>
2025-11-04 10:02:16 -0600

    actualizacion y correccion pero el resultado es erroneo y esta en correccion 04/11/2025

--- Arreglos.js
+++ Arreglos.js
@@ -10,12 +10,14 @@ array
 function
 */
 function DatosArreglos(){
-let Datos=[3,7,9,3,2];
-let suma=0;
-for (i=0;i<Datos.length;i++){
-    if ((Datos[i])%!==0){
-    suma +=Datos[i];
-    console.log(`la posicion es ${i} y el valor es ${Datos[i]}`);
+    const Datos = [3,7,9,3,2];
+    let suma = 0;
+    for (let i = 0; i < Datos.length; i++){
+        if (Datos[i] % 2 === 0) {
+            suma += Datos[i];
+            console.log(`la posicion es ${i} y el valor es ${Datos[i]}`);
+        }
     }
+    console.log(`la suma de los arreglos es: ${suma}`);
 }
 DatosArreglos();
