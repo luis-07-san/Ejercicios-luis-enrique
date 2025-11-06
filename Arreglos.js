0e2ab33a7decc7f1cda06967b4f6b8e941a3763a
luis enrique <luisenriquesanchezjimenez689@gmail.com>
2025-11-06 09:01:51 -0600

    actualizacion y correccion realizado 06/11/2025

--- Arreglos.js
+++ Arreglos.js
@@ -1,22 +1,22 @@
 //declaraccion de arreglos 
 /* tipos de datos
-number 
-string 
-boolean
-null
-undifined
-object
-array
-function
+Number 
+String 
+Boolean
+Null
+Undifined
+Object
+Array
+Function 
 */
 function DatosArreglos(){
-    const Datos = [3,7,9,3,2];
+    let Datos = [3,7,9,3,2];
     let suma = 0;
-    for (let i = 0; i < Datos.length; i++){
-        if (Datos[i] % 2 === 0) {
+    for ( i = 0; i < Datos.length; i++){
+        if ((Datos[i] ) %2 ===0) ;
             suma += Datos[i];
             console.log(`la posicion es ${i} y el valor es ${Datos[i]}`);
-        }
+        //console.log(`la posicion es ${i} y el valor es ${Datos[i]}`);
     }
     console.log(`la suma de los arreglos es: ${suma}`);
 }
